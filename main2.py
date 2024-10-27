d1063dae2f608d8f9fee4be24f6c450c2a335646
Asajadafsar <smyhqasmyan2@gmail.com>
2024-10-27 05:17:57 -0700

    add pakahe I do Not install!

--- main2.py
+++ main2.py
@@ -1,69 +1,50 @@
 import time
-import json
-from seleniumwire import webdriver  # برای رهگیری درخواست‌ها
+from seleniumwire import webdriver  # For request interception
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from datetime import datetime  # برای ثبت زمان
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-# تنظیمات کروم برای اندروید
+
+# Chrome options
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--no-proxy-server')  # غیرفعال کردن استفاده از پروکسی
 
 
-#اینجا مررورگر نصب میکنه و دانلود میکنه
+# Setup Chrome driver
 service = Service(ChromeDriverManager().install())
-
-#اینجا تنظیمات که برای مرورگر تنظیم کردیم و فایل مروگر وارد میکنه دارایور میسازه اماده میکنه
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-
-#این تابع برای کلیک
+# Function to click elements by XPath
 def click_element(xpath):
     element = driver.find_element(By.XPATH, xpath)
     element.click()
 
-# تابعی برای تایپ کردن بر اساس XPath
+# Function to type text into elements by XPath
 def type_in_element(xpath, text):
     element = driver.find_element(By.XPATH, xpath)
     element.send_keys(text)
 
-
-#اینم سایت برا اول باز میکنه
+# Open the website
 driver.get('https://m.snappfood.org/')
-
 time.sleep(5)
 
-
-#کلیک روی انتخب نقشه
+# Click on select map
 click_element('/html/body/div[2]/div[1]/div/div/div[2]/button')
-
 time.sleep(2)
 
-#کلیک روی دکمه ورود در منو پایین
+# Click on login button in the bottom menu
 click_element('/html/body/div[1]/div[3]/div')
 time.sleep(0.5)
 
-
-#کلیک روی دکمه عضویت
+# Click on register button
 click_element('/html/body/div[1]/div[4]/div/div/div[3]/button')
 time.sleep(0.5)
 
-
-#کلیک روی اینپوت شماره همراه
+# Click on mobile input field
 click_element('/html/body/div[1]/div[2]/section/div[1]/div[3]/div[1]/input')
 time.sleep(0.5)
 
-#تایپ شماره همراه روی اینپوت شماره همراه
-type_in_element('/html/body/div[1]/div[2]/section/div[1]/div[3]/div[1]/input','09000000')
-
+# Type mobile number into input field
+type_in_element('/html/body/div[1]/div[2]/section/div[1]/div[3]/div[1]/input', '09000000')
 time.sleep(10)
 
-
-
-
-
-
-
-
-
+# Close the browser
+driver.quit()
